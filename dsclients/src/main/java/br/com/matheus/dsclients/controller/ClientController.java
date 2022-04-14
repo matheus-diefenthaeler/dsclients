a0b00a6f80f2edfc709c66636d39a9d94e4d789e
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-04-13 22:36:18 -0300

    Fix ClientService, ClientController

--- dsclients/src/main/java/br/com/matheus/dsclients/controller/ClientController.java
+++ dsclients/src/main/java/br/com/matheus/dsclients/controller/ClientController.java
@@ -4,21 +4,22 @@ import br.com.matheus.dsclients.entities.Client;
 import br.com.matheus.dsclients.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(name = "/clients")
-public class clientController {
+@RequestMapping(value = "/clients")
+public class ClientController {
 
     @Autowired
     private ClientService service;
 
+    @GetMapping
     public ResponseEntity<List<Client>> findAll() {
-        List<Client> list = new ArrayList<>();
+        List<Client> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
--- dsclients/src/main/java/br/com/matheus/dsclients/services/ClientService.java
+++ dsclients/src/main/java/br/com/matheus/dsclients/services/ClientService.java
@@ -4,6 +4,7 @@ import br.com.matheus.dsclients.entities.Client;
 import br.com.matheus.dsclients.respository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Client> findAll() {
         return repository.findAll();
     }
