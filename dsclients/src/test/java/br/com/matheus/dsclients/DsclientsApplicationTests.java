61d07734665750299bb16a6c0abfe45873e33438
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-04-13 21:38:05 -0300

    Creating project

---
+++ dsclients/src/test/java/br/com/matheus/dsclients/DsclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus.dsclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
