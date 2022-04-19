f08657ea8dca4f3c3e2fc3ebc77a65254acc1611
Matheus Diefenthaeler <matheus.axo@gmail.com>
2022-04-18 23:16:26 -0300

    Auditing data

--- dsclients/src/main/java/br/com/matheus/dsclients/entities/Client.java
+++ dsclients/src/main/java/br/com/matheus/dsclients/entities/Client.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.Instant;
@@ -30,6 +33,22 @@ public class Client implements Serializable {
     private Double income;
     private Instant birthDate;
     private Integer children;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
+    }
 
     @Override
     public boolean equals(Object o) {
